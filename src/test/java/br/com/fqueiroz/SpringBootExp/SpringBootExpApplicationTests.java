6a340b882c6e0e423ce2d23ecf1b1c4a2db85f04
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2025-01-30 17:55:26 -0300

    primeiro commit

---
+++ src/test/java/br/com/fqueiroz/SpringBootExp/SpringBootExpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fqueiroz.SpringBootExp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
